1f448f21ae23811143759bb08369b4d016518a8e
abdullahkocak123 <abdullahkocak33@hotmail.com>
2024-07-13 17:32:46 +0300

    Added Polindrom example

---
+++ src/Polindrom.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Polindrom {
+    public boolean isPolindrom (String str){
+        char[] list = str.toCharArray();
+        for (int i=0; i< list.length; i++){
+            if (list[i]!=list[list.length - i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}

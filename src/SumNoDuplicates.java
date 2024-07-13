165cc9b60353ff485f2de1a880ae058b6c9db6cd
abdullahkocak123 <abdullahkocak33@hotmail.com>
2024-07-13 13:49:27 +0300

    Added reformat

--- src/SumNoDuplicates.java
+++ src/SumNoDuplicates.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
 public class SumNoDuplicates {
-    public int sumNoDuplicates (int[] arr){
-        int result=0;
+    public int sumNoDuplicates(int[] arr) {
+        int result = 0;
         ArrayList<Integer> newList = new ArrayList<>();
-        for (int i=0; i< arr.length;i++) {
+        for (int i = 0; i < arr.length; i++) {
             boolean match = false;
-            for (int j=0; j< arr.length; j++) {
-                if (i != j && arr[i]==arr[j]) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && arr[i] == arr[j]) {
                     match = true;
                     break;
                 }

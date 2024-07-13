05967305ad4f772a92a9e1a3db4f082ade65673e
abdullahkocak123 <abdullahkocak33@hotmail.com>
2024-07-13 14:32:52 +0300

    Added VowelCount example

---
+++ src/VowelCount.java
@@ -0,0 +1,11 @@
+public class VowelCount {
+    public int getVowelCount(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'|| str.charAt(i) == 'u'){
+                count++;
+            }
+        }
+        return count;
+    }
+}
